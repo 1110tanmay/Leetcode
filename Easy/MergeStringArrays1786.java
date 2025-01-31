5f3476cda7f5ac243113b491b7fb38b4da2e4738
1110tanmay <1110tanmay@gmail.com>
2025-01-30 18:36:02 -0700

    Broke strings into arrays and concatenated the strings again. Figuring how to add character one by one.

--- Easy/MergeStringArrays1786.java
+++ Easy/MergeStringArrays1786.java
@@ -1,5 +1,17 @@
 public class MergeStringArrays1786{
     public static void main(String[] args) {
-        System.out.println("Testing");
+        String word1 = "tanmay";
+        String word2 = "shelar";
+        int strlen = word1.length();
+        int strlen2 = word2.length();
+        for(int i = 0;i<strlen;i++){
+            System.out.println(word1.charAt(i));
+        }
+        for (int j=0;j<strlen2;j++){
+            System.err.println(word2.charAt(j));
+        }
+        System.out.println(strlen);
+        System.out.println(strlen2);
+        System.out.println(word1.concat(word2));
     }
 }
